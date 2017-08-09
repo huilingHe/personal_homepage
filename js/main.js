2cd1bebd4a69183ff769106b1a3bc727f3508552
huilingHe <1946772492@qq.com>
2017-08-09 15:22:41 +0800

    修改动态加载图

--- js/main.js
+++ js/main.js
@@ -7,44 +7,42 @@
 //   });
 // });
 window.onload = function () {
-
-
-$(document).ready(function () {
-  $('#right-roll').fullpage({
-    slidesColor: ['#fff', '#fff', '#fff', '#fff', '#fff'],
-    anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
-    menu: '#menu',
-    continuousVertical: true,
-    afterLoad: function (anchorLink, index) {
-      if (index == 3) {
-        $('.professional-skills').find('h2').delay(300).animate({
-          right: '0'
-        }, 800, 'easeOutExpo');
-        $('.professional-skills').find('.skills-list').delay(300).animate({
-          left: '0'
-        }, 800, 'easeOutExpo');
+  $(document).ready(function () {
+    $('#right-roll').fullpage({
+      slidesColor: ['#fff', '#fff', '#fff', '#fff', '#fff'],
+      anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+      menu: '#menu',
+      continuousVertical: true,
+      afterLoad: function (anchorLink, index) {
+        if (index == 3) {
+          $('.professional-skills').find('h2').delay(300).animate({
+            right: '0'
+          }, 800, 'easeOutExpo');
+          $('.professional-skills').find('.skills-list').delay(300).animate({
+            left: '0'
+          }, 800, 'easeOutExpo');
+        }
+        if (index == 4) {
+          $('.work-experience').find('h2').delay(300).animate({
+            left: '0'
+          }, 800, 'easeOutExpo');
+        }
+      },
+      onLeave: function (index, direction) {
+        if (index == '3') {
+          $('.professional-skills').find('h2').delay(300).animate({
+            right: '-300%'
+          }, 800, 'easeOutExpo');
+          $('.professional-skills').find('.skills-list').delay(300).animate({
+            left: '-260%'
+          }, 800, 'easeOutExpo');
+        }
+        if (index == '4') {
+          $('.work-experience').find('h2').delay(300).animate({
+            left: '-380%'
+          }, 800, 'easeOutExpo');
+        }
       }
-      if (index == 4) {
-        $('.work-experience').find('h2').delay(300).animate({
-          left: '0'
-        }, 800, 'easeOutExpo');
-      }
-    },
-    onLeave: function (index, direction) {
-      if (index == '3') {
-        $('.professional-skills').find('h2').delay(300).animate({
-          right: '-300%'
-        }, 800, 'easeOutExpo');
-        $('.professional-skills').find('.skills-list').delay(300).animate({
-          left: '-260%'
-        }, 800, 'easeOutExpo');
-      }
-      if (index == '4') {
-        $('.work-experience').find('h2').delay(300).animate({
-          left: '-380%'
-        }, 800, 'easeOutExpo');
-      }
-    }
+    });
   });
-});
 }
